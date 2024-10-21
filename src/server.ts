47b7f6fec0b53994736135728ce0535eaff1d3f3
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-21 19:35:51 +0600

    project creation done

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,5 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import projectRoutes from "./routes/projectRoutes";
-// import dotenv from "dotenv";
 
-
-
-// dotenv.config();
-
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-
-// app.use("/api", projectRoutes);
-// app.get("/", (req, res) => {
-//   res.send("Welcome to the portfolio dashboard backend system");
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// mongoose
-//   .connect(process.env.MONGO_URI as string)
-//   .then(() => {
-//     app.listen(PORT, () =>
-//       console.log(`Server running on port      
-//     ${PORT}`)
-//     );
-//   })
-//   .catch((error) => console.log(error.message));
-
-
-
-
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import projectRoutes from "./routes/projectRoutes";
@@ -52,19 +19,17 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", projectRoutes);
-
-app.get('/test', (req: Request, res: Response) => {
-  req.user = { id: 1, name: 'Test User' }; // Test setting the user property
-  res.json(req.user);
+app.get("/", (req, res) => {
+  res.send("Welcome to the portfolio dashboard backend system");
 });
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI as string)
   .then(() => {
     app.listen(PORT, () =>
-      console.log(`Server running on port      
-    ${PORT}`)
+      console.log(`Server running on port ${PORT}`)
     );
   })
   .catch((error) => console.log(error.message));
